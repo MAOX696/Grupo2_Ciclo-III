ecb5af7fc2e9401b8d8d1adb0ed312e00cb13adc
Joel Leonardo <leoncc2@hotmail.com>
2022-09-20 17:50:34 -0500

    Spring

---
+++ JS/Inditex.js
@@ -0,0 +1,30 @@
+function verTienda(){
+    var  listaTienda= [
+       
+     ];
+     
+function verQuienes(){
+    var  listaTienda= [
+        ssddd   
+    ];
+      
+function verContacto(){
+    var  listaTienda= [
+               
+    ];
+
+function validar_formulario(){
+    var username=document.registrouser.username;
+    var email=document.registrouser.correo;
+    var passw=document.registrouser.passw;
+
+    alert("Estoy ingresando a la función de validar");
+}
+
+function mensaje_correo(){
+    alert("Estas en el campo correo");
+}
+
+function mensaje_enviado(){
+    alert("Información Enviada")
+}
